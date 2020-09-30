920bc84be64a5f0992c5856c0c234827e7979a36
Alexei Soliz <alexsr94@gmail.com>
2020-09-30 07:38:22 +0200

    make it better

---
+++ precache-manifest.56a3d50d6eec4ea042b6dacdea9dd5b7.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b002bdd0a7289b737fe21f6a205d439f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cd4a7be6246dffbb15c",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4912efcb7ca078403e41",
+    "url": "/static/css/main.1e05c6ca.chunk.css"
+  },
+  {
+    "revision": "5cd4a7be6246dffbb15c",
+    "url": "/static/js/2.1f212654.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1f212654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4912efcb7ca078403e41",
+    "url": "/static/js/main.5ca518ed.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "d88100aad93ff03d4526cb633084029b",
+    "url": "/static/media/blucircleLogin.d88100aa.png"
+  },
+  {
+    "revision": "971f4bea66d786b4fa497a54bad00bf7",
+    "url": "/static/media/dashboardImg.971f4bea.jpg"
+  },
+  {
+    "revision": "6125bc9113fe7b82ccea4a897ab3dea0",
+    "url": "/static/media/errorIcon.6125bc91.png"
+  },
+  {
+    "revision": "5fb7951c3cdbb72bbbe98f67daf4b367",
+    "url": "/static/media/fistStepRightImgBg.5fb7951c.png"
+  },
+  {
+    "revision": "738579340e2aaf358f1db61eba93549d",
+    "url": "/static/media/fullControl.73857934.png"
+  },
+  {
+    "revision": "598c9fde2d100b51a754892c15affbd7",
+    "url": "/static/media/getAccess.598c9fde.jpg"
+  },
+  {
+    "revision": "9309b339d6c21ea8d8f5e81a1ea90f52",
+    "url": "/static/media/home.9309b339.png"
+  },
+  {
+    "revision": "39ab7dc83569dcf96be19ce1cce1b11a",
+    "url": "/static/media/loginRightElement.39ab7dc8.png"
+  },
+  {
+    "revision": "15975a7f066d5c7e95a1d5be72b529e9",
+    "url": "/static/media/nav-bg-element.15975a7f.png"
+  },
+  {
+    "revision": "fd53c6aa75b3cb09e0e1996cb949c253",
+    "url": "/static/media/profile-dummy.fd53c6aa.png"
+  },
+  {
+    "revision": "00a0510bda145da46c891db5290b7938",
+    "url": "/static/media/signup-bg-element.00a0510b.png"
+  },
+  {
+    "revision": "520d1b8619488c80860aef1fea9a1518",
+    "url": "/static/media/signuptopcircleblue.520d1b86.png"
+  },
+  {
+    "revision": "40866dfe8393bd9efac0f433928e82a5",
+    "url": "/static/media/successLogin.40866dfe.png"
+  }
+]);
